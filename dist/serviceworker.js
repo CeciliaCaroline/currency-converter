ef7216bba065c4383c434a7e88f053dbe9916fc9
CeciliaCaroline <nalubegac58@gmail.com>
2018-07-01 22:44:23 +0300

    Cache bundle.js

--- dist/serviceworker.js
+++ dist/serviceworker.js
@@ -4,7 +4,8 @@ const cacheFiles = [
   "../static/img/cb.jpeg",
   "../static/styles.css",
   "../converter.html",
-  "../dist/index.html"
+  "../dist/index.html",
+  "../dist/bundle.js"
 ];
 
 self.addEventListener("install", e => {
@@ -16,25 +17,6 @@ self.addEventListener("install", e => {
 });
 
 self.addEventListener("fetch", event => {
-  // e.respondWith(
-  //   caches.open(cacheName).then(cache => {
-  //     caches.match(e.request).then(response => {
-  //       if (response) {
-  //         console.log("response", response);
-  //         console.log("[Service worker] Found in Cache", e.request.url);
-  //         return response;
-  //       }
-
-  //       return fetch(e.request).then(response => {
-  //         if (response) {
-  //           let responseClone = response.clone();
-  //           cache.put(e.request, responseClone);
-  //           return response;
-  //         }
-  //       });
-  //     });
-  //   })
-  // );
   event.respondWith(
     caches.open(cacheName).then((cache) => {
       return cache.match(event.request).then((response) => {
