d2593f29d58548e38a96f60dc98c6806690dd8a4
CeciliaCaroline <nalubegac58@gmail.com>
2018-07-01 20:34:30 +0300

    Persist cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,10 +1,11 @@
 const cacheName = "cachev1";
 const cacheFiles = [
-  "../converter.js",
-  "../static/img/cb.jpeg",
-  "../static/styles.css",
-  "../converter.html",
-  "../dist/index.html"
+  "./converter.js",
+  "./static/img/cb.jpeg",
+  "./static/styles.css",
+  "./converter.html",
+  "./index.html",
+  "./converter.html"
 ];
 
 self.addEventListener("install", e => {
